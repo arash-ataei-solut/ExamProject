0e96532863560de19646325261cffc9775c61ee6
arash <arash.ataee76@yahoo.com>
2020-01-27 12:04:03 +0330

    signup apiview

--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 from rest_framework.viewsets import ViewSet, ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+
 
 from .models import Profile
 from .serializers import ProfileSerializer, ProfileDetailsSerializer
@@ -15,3 +20,15 @@ class ProfileViewSet(ReadOnlyModelViewSet):
 
     def get_serializer_class(self):
         return self.serializers.get(self.action)
+
+
+class Signup(APIView):
+
+    def post(self, request):
+        print(request.data.get('username'))
+        user, _ = User.objects.get_or_create(username=request.data.get('username'))
+        user.set_password(request.data.get('password'))
+        user.save()
+        profile, _ = Profile.objects.get_or_create(user=user)
+        token, _ = Token.objects.get_or_create(user=user)
+        return HttpResponse('ok')
