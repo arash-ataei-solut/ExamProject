60c9027fc1850781da4bcd9d2792334d4286933f
arash <arash.ataee76@yahoo.com>
2020-01-27 12:05:31 +0330

    signup url

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from .views import ProfileViewSet
+from .views import ProfileViewSet, Signup
 
 
 router = SimpleRouter()
@@ -9,4 +9,6 @@ router = SimpleRouter()
 router.register('profile', ProfileViewSet)
 
 
-urlpatterns = []
+urlpatterns = [
+    path('signup/', Signup)
+]
